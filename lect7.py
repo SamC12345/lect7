994a60e399daebdd2f3b7c06818af7f567523007
Sam <student979@live.com>
2020-09-23 15:10:04 -0400

    add origin files to master

---
+++ lect7.py
@@ -0,0 +1,16 @@
+import flask
+import os
+import random
+
+app = flask.Flask(__name__)
+artists=[("Halsey","halsey.jpg"),("J Cole","jcole.jpg"),("TheFatRat","fatrat.jpg")]
+@app.route('/')
+def index():
+    print("in indexsdf")
+    return flask.render_template("index.html", len=len(artists), artists=artists)
+    
+app.run(
+    port=int(os.getenv('PORT', 8080)),
+    host=os.getenv('IP', '0.0.0.0'),
+    debug=True
+)
